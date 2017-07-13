df6cdadb5941b0afa562e056fc894207db20197e
DunnFair <e638097@hotmail.com>
2017-07-13 22:28:14 +0800

    新增寶可夢類別庫

--- HWPokemon/Pokemon01/Program.cs
+++ HWPokemon/Pokemon01/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PokemonLibrary;
+
 
 namespace Pokemon01
 {
@@ -10,6 +12,30 @@ namespace Pokemon01
     {
         static void Main(string[] args)
         {
+            Pokemon item = new Pokemon()
+            {
+                name = "妙蛙種子",
+                hp = 60,
+                kg = 5.94,
+                gender = "男",
+                type = "草/毒",
+                height = 67,
+                skill01 = "藤編",
+                skill02 = "汙泥炸彈",
+                skill01damage = 7,
+                skill02damage = 80,
+                Candy = 27,
+                Star = 271484,
+                Strong = 3000,
+                EVO = 25
+            };
+            Console.WriteLine("名稱:{0},血量{1},體重:{2},性別:{3},屬性:{4},身高:{5},技能1:{6},技能2:{7},藤鞭傷害:{8},汙泥炸彈:{9}"
+                , item.name, item.hp, item.kg, item.gender, item.type, item.height, item.skill01, item.skill02
+                ,item.skill01damage,item.skill02damage
+                );
+
+            Console.WriteLine("==========================================================================");
+            Console.WriteLine("糖果:{0},星星:{1},強化需求:{2},進化需求:{3}", item.Candy, item.Star, item.Strong, item.EVO);
         }
     }
 }
---
+++ PokemonLibrary/PokemonLibrary/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string name;
+        public int hp;
+        public double kg;
+        public string gender;
+        public string type;
+        public double height;
+        public string skill01;
+        public string skill02;
+        public int Candy;
+        public int Star;
+        public int Strong;
+        public int EVO;
+        public string Address;
+        public int skill01damage;
+        public int skill02damage;
+    }
+}
